cf937c291eab633908772c5caae0a32e3d9b8f2a
owais46 <owaisrizwan2@gmail.com>
2025-09-05 02:05:29 +0500

    icon imports

--- components/Layout/Navbar.tsx
+++ components/Layout/Navbar.tsx
@@ -12,7 +12,8 @@ import {
   UserGroupIcon,
   ChartBarIcon,
   DocumentTextIcon,
-  CubeIcon
+  CubeIcon,
+  ChatBubbleLeftRightIcon
 } from '@heroicons/react/24/outline';
 import { motion, AnimatePresence } from 'framer-motion';
 
